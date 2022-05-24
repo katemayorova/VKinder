6cc4c391c57b9fba1f81209a3172793031995630
katemayorova <katemayorova0@gmail.com>
2022-05-24 20:51:07 +0300

    Make User a dataclass

--- user.py
+++ user.py
@@ -1,19 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class User:
-    def __init__(self,
-                 user_id: str,
-                 name: str,
-                 surname: str,
-                 city: str,
-                 sex: str,
-                 relation: str,
-                 top_photos=None
-                 ):
-        if top_photos is None:
-            top_photos = []
-        self.id = user_id
-        self.name = name
-        self.surname = surname
-        self.top_photos = top_photos
-        self.city = city
-        self.sex = sex
-        self.relation = relation
+    vk_id: str
+    name: str
+    surname: str
+    city: str
+    sex: str
+    relation: str
+    top_photos = None
