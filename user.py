3f247f8f2b3bbdd608eb9972739ce56e5a0cef62
katemayorova <katemayorova0@gmail.com>
2022-05-31 21:20:12 +0300

    Implement dictionaries SEX_MAPPING and SEX_MAPPING_REVERSE

--- user.py
+++ user.py
@@ -3,6 +3,14 @@ from dataclasses import dataclass
 
 @dataclass
 class User:
+    SEX_MAPPING = {
+        "м": 2,
+        "ж": 1
+    }
+    SEX_MAPPING_REVERSE = {
+        1: "ж",
+        2: "м"
+    }
     vk_id: str
     name: str
     surname: str
